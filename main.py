11e0d3e9c2bc0dd2d4859f19f54cd2da103e89f2
benjamin-dao <daominhphuc05@gmail.com>
2025-06-05 15:15:01 +0700

    feat: word count

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_num_words
+from stats import *
+import sys
+
 
 # A with block can be used to open a file:
 # The with block will automatically close the file when the block is exited, cleaning up resources.
@@ -10,7 +12,27 @@ def get_book_text(path_to_file):
 
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
-    get_num_words(file_contents)
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 main.py <path_to_book>")
+    print("============ BOOKBOT ============")
+    path_to_file = sys.argv[1]
+    print(f"Analyzing book found at {path_to_file}")
+
+    print("----------- Word Count ----------")
+    file_contents = get_book_text(path_to_file)
+    num_words = get_num_words(file_contents)
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+    word_count = count_char(file_contents)
+    
+    word_count_list = sort_word_count(word_count)
+    for pair in word_count_list:
+        if pair["char"].isalpha() == False:
+            continue
+        print(pair["char"] + ":",pair["num"])
+
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,26 @@
 
 def get_num_words(file_contents):
     num_words = len(file_contents.split())
-    print(f"{num_words} words found in the document")
+    return num_words
+
+def count_char(file_contents):
+    word_count = {}
+    for char in file_contents.lower():
+        if char in word_count:
+            word_count[char] += 1
+        else:
+            word_count[char] = 1
+    return word_count
+
+def sort_on(unsorted_word_count):
+    return unsorted_word_count["num"]
+
+def sort_word_count(word_count):
+    sort_list = []
+    for key, value in word_count.items():
+        sort_list.append({"char": key, "num": value})
+    sort_list.sort(reverse=True, key=sort_on)
+    return sort_list
+
+
+
